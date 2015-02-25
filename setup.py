fbac91146b7700689ba90b549e6fc1a308241982
Zaytsev Dmitriy <still65@yandex.ru>
2015-02-25 17:31:34 +0600

    add stup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Bowling',
+      version='1.0',
+      description='Python module to count bowling scores',
+      author='Zaytsev Dmitriy',
+      license="BSD",
+      author_email='gward@python.net',
+      packages=['bowling', 'tests']
+)
